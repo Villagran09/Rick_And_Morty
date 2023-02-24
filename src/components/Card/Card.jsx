8f79d92c1eef51e92fc280ce7ecbf206ded9c4c0
Miguel <miguevillagran@gmail.com>
2023-02-24 18:54:35 -0300

    react-redux commit two

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -41,10 +41,10 @@ export default function Card({ name, species, gender, image, onClose, id }) {
       </button>
       <Link to={`/detail/${id}`}>
         <h2 >{name}</h2>
+      </Link>
         <h2 >{species}</h2>
         <h2 >{gender}</h2>
         <img src={image} alt={name} />
-      </Link>
     </div>
   );
 }
